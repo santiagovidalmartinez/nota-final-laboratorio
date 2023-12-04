6110e55f6d9fe43e3c2f2705ea6b3ca93b0c05fc
santiagovidalmartinez <142599572+santiagovidalmartinez@users.noreply.github.com>
2023-12-04 15:11:12 -0500

    Update Automovil.java

--- Parcial-II-main/Parcial-II-main/Parcial2/src/main/java/Taller/Automovil.java
+++ Parcial-II-main/Parcial-II-main/Parcial2/src/main/java/Taller/Automovil.java
@@ -1,4 +1,10 @@
 package Taller;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
 public class Automovil extends Vehiculo{
     private int numeroPuertas;
 
@@ -6,12 +12,23 @@ public class Automovil extends Vehiculo{
         super(marca, modelo, placa,horaEntrada);
         this.numeroPuertas = numeroPuertas;
     }
-    
+
     public int getNumeroPuertas() {
         return numeroPuertas;
     }
 
     public void setNumeroPuertas(int numeroPuertas) {
         this.numeroPuertas = numeroPuertas;
+    }
+    
+    public void guardarAuto (Automovil auto) throws IOException{
+        try {
+            FileOutputStream fichero;
+            fichero = new FileOutputStream("auto.txt");
+            ObjectOutputStream crear = new ObjectOutputStream(fichero);
+            crear.writeObject(auto);
+        } catch (FileNotFoundException ex) {
+            System.out.println("\n033[32mA ocurrido un problema con el fichero");;
+        }
     }
 }
