c9ee7d233fb024828de62420abd8c6e283d89d22
santiagovidalmartinez <142599572+santiagovidalmartinez@users.noreply.github.com>
2023-12-04 15:12:19 -0500

    Update Motocicleta.java

--- Parcial-II-main/Parcial-II-main/Parcial2/src/main/java/Taller/Motocicleta.java
+++ Parcial-II-main/Parcial-II-main/Parcial2/src/main/java/Taller/Motocicleta.java
@@ -1,4 +1,10 @@
 package Taller;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
 public class Motocicleta extends Vehiculo{
  private int cilindrada;
 
@@ -9,12 +15,21 @@ public class Motocicleta extends Vehiculo{
     public void setCilindrada(int cilindrada) {
         this.cilindrada = cilindrada;
     }
-    
+
 
     public Motocicleta(String marca, String modelo, String placa,int cilindrada,int horaEntrada, int horaSalida) {
         super(marca, modelo, placa, horaEntrada);
         this.cilindrada = cilindrada;
     }
-    
-    
+
+    public void guardarMoto (Motocicleta moto) throws IOException{
+            try {
+                FileOutputStream fichero;
+                fichero = new FileOutputStream("moto.txt");
+                ObjectOutputStream crear = new ObjectOutputStream(fichero);
+                crear.writeObject(moto);
+            } catch (FileNotFoundException ex) {
+                System.out.println("\n033[32mA ocurrido un problema con el fichero");;
+            }
+    }
 }
